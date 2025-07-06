e1a268c8ddd381e34b23b729a79941c90c4d3240
BerkeAnna <berke.anna0803@gmail.com>
2025-07-06 16:23:19 +0200

    new todo without saving

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -2,6 +2,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -9,7 +10,7 @@ import { CommonModule } from '@angular/common';
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss',
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, FormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoListComponent {
@@ -20,7 +21,24 @@ export class TodoListComponent {
     { id: 4, text: 'fourth todo', done: false }
   ];
 
+  newTodoText = '';
+
   toggleDone(index: number): void{
     this.todos[index].done = !this.todos[index].done;
+  }
+
+  addTodo(): void {
+    const text = this.newTodoText.trim();
+    if(text){
+      const nextId = this.todos.length > 0 ? Math.max(...this.todos.map(t => t.id)) + 1 : 1;
+
+      this.todos.unshift({
+        id: nextId,
+        text,
+        done: false
+      });
+
+      this.newTodoText = '';
+    }
   }
 }
